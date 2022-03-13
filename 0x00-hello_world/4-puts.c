35466a48bcc8f41e78dbeb7891475e9ddab182dc
Adurota01 <adurotapeter@gmail.com>
2022-03-13 23:07:13 +0100

    my commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *  main - prints the phrase"programming[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C [C[D"programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+        puts ("/"[3~[3~[\"Programming is like builing a multilingual puzzle");
+        return (0);
+}
