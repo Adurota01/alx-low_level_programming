8bae21d8aea83995a2291fc0481eb556cc736819
Adurota01 <adurotapeter@gmail.com>
2022-03-13 23:22:45 +0100

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 /**
- *  main - prints the phrase"programming[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C [C[D"programming is like building a multilingual puzzle
+ * main - prints the phrase "Programming is like building a multilingual puzzle
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-        puts ("/"[3~[3~[\"Programming is like builing a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+© 2022 GitHub, Inc.
+Terms
+[A[C[3~[3~[3~[[C[C[C[A[C[C[C[C[C[C[C[C[C[
