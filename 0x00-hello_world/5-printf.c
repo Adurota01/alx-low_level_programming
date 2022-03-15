f56f608168015428b7d599ee0186e9dc1b92192b
Adurota01 <adurotapeter@gmail.com>
2022-03-15 04:02:31 +0100

    my commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - prints the phrase with proper grammar
-[A * main  prints the phrase with proper grammar,
- * but the outcome is a [iece of art[D[D[D[D[D[D[D[D[D[D[p[C[C[C[C[C[C[C[C[C[C[C,
- * Return: 0 if exited properly. non-zero otherwise
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-         printf("with proper grammar, but the outcome is a piece of art,/[3~\[3~[\n");
-         return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
