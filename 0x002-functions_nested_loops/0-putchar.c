c06cc7e6bb1eb796ad4e1ec579754ccbd3099f70
Adurota01 <adurotapeter@gmail.com>
2022-03-16 19:58:02 +0100

    my commit

---
+++ 0x002-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ * 
+ * Return: 0 (success)[D[D[D[D[D[D[S[C[C[C[C[C[C[C
+ */
+
+int main(void)
+{
+        char text[9] = "_putchar";
+        int i = 0;
+ 
+        for (i = 0;i < 8;i++[D[D[D i++)
+        {
+               _putchar(text[i]);
+        }
+        _putchar('\n');[D[C
+
+        return (0);
+}
---
+++ 0x002-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+readme file for 0x02-funtions_nessted_loops[D[D[D[D[D[D[Dted_[3~[3~[3~[3~[3~loops
+ls
+[A[C
